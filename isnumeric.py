35b24f8eadc97db06126a786c321172151342f2c
0II <jacklahvh@gmail.com>
2020-06-02 03:22:17 +0100

    🌠

--- isnumeric.py
+++ isnumeric.py
@@ -2,3 +2,7 @@ txt = "5656218"
 
 print(txt.isnumeric())
 
+ntxt = "5656218F"
+
+print(ntxt.isnumeric())
+
---
+++ isupper.py
@@ -0,0 +1,7 @@
+txt = "THIS IS NOW!"
+
+print(txt.isupper())
+
+ltxt = "THIS IS NOw!"
+
+print(ltxt.isupper())
