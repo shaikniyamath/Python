3eb8c8d07dc49f59a7a8a64ca70b8edbb4224ad8
Jack <jpwww@protonmail.com>
2020-06-09 17:00:02 +0100

    🌠

--- booleans.py
+++ booleans.py
@@ -4,4 +4,4 @@ y = 42
 print(bool(x)) #prints true
 print(bool(y)) #prints true
 
-#both statements print true because the interpreter only checks if the variables have values other than False, None.
+# Both statements print true because the interpreter only checks if the variables have values other than False, None.
