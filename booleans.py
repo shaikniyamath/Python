c4f4722fd495c4ec13719e33f2b0b28427ab6da2
Zander Kraig <66487710+ZanderKraig17@users.noreply.github.com>
2020-06-09 18:14:41 +0530

    Update booleans.py
    Added comments to explain the output

--- booleans.py
+++ booleans.py
@@ -1,5 +1,7 @@
 x = "Hi"
 y = 42
 
-print(bool(x))
-print(bool(y))
+print(bool(x)) #prints true
+print(bool(y)) #prints true
+
+#both statements print true because the interpreter only checks if the variables have values other than False, None.
