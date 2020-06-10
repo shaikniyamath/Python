00bf7751587f9cbf016621649ee1b1a85b0f62a8
Jack <jpwww@protonmail.com>
2020-06-10 04:48:04 +0100

    🌠

--- print.py
+++ print.py
@@ -1,6 +1,6 @@
 print("This is Python!")
 print("Python is an", "object-oriented", " programming language.") # By separating the strings with commas, 
-                                                                   # one space will be placed in between consecutive strings.
+                                                                   # one space will be placed in between consecutive strings. 
 num = 4 
 print("The value of the variable num is: " + str(num) + ".") # Here the + operator is used to concatenate strings. No spaces are added
                                                              # between the strings. str(num) coverts an integer to a string.
